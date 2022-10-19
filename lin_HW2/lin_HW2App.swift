cc04ab84bd3029374d749e791553432f7a46a940
tony <linchiah01@gmail.com>
2022-10-19 14:01:54 +0800

    Initial Commit

---
+++ lin_HW2/lin_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  lin_HW2App.swift
+//  lin_HW2
+//
+//  Created by User01 on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct lin_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
